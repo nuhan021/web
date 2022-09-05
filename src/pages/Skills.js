ab6a69dfb75d183aa54c8e6fc88ed794c2d30e68
nuhan021 <96333666+nuhan021@users.noreply.github.com>
2022-09-05 22:45:00 +0600

    Create Skills.js

---
+++ src/pages/Skills.js
@@ -0,0 +1,11 @@
+const Skills =  ()=>{
+    return(
+        <>
+        <div className="container">
+            <h1>Upcomming content</h1>
+        </div>
+        </>
+    )
+}
+
+export default Skills;
